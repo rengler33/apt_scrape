4d561023505ebc2e5862a4e3d0c9d891f7773719
Robert Engler <rengler33@gmail.com>
2021-06-19 14:53:38 -0500

    Add comments

--- streeteasy/streeteasy/spiders/build_gql_query.py
+++ streeteasy/streeteasy/spiders/build_gql_query.py
@@ -4,20 +4,24 @@ from pathlib import Path
 import httpx 
 
 headers = {
-    'Origin': 'https://streeteasy.com',
+    'Origin': 'https://streeteasy.com',  # seems to be optional
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
     'content-type': 'application/json',
 }
 
 with open("streeteasy/ids.jl") as f:
     ids = json.loads(f.read())["ids"]
-ids = ids[0]
+ids = ids[0]  # take only first one so as not to spam the server
+
+# introspection seems not allowed
+# with open(Path(__file__).parent / "introspection.graphql") as f:
+#     query = f.read()
 
 with open(Path(__file__).parent / "rental_query.graphql") as f:
     query = f.read()
     
 data_dict = {
-    "operationName":"rentals",
+    # "operationName":"rentals",  # seems to be optional
     "variables": {"ids": ids},
     "query": query,
 }
@@ -28,8 +32,10 @@ response = httpx.post(
     'https://api-internal.streeteasy.com/graphql', 
     headers=headers,
     content=data_raw,
-    timeout=None,
+    timeout=10,  # longer timeout needed if sending lots of IDs
 )
 
-print(response.text)
+data = json.loads(response.text)
+
+print(json.dumps(data, indent=2))
 
