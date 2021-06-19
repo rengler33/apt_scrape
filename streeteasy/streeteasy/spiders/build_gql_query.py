dfd4bd722491d9f07efec244b358fb9e16bb9f0a
Robert Engler <rengler33@gmail.com>
2021-06-19 15:08:10 -0500

    Refactor to have separate building query

--- streeteasy/streeteasy/spiders/build_gql_query.py
+++ streeteasy/streeteasy/spiders/build_gql_query.py
@@ -3,39 +3,65 @@ from pathlib import Path
 
 import httpx 
 
-headers = {
-    'Origin': 'https://streeteasy.com',  # seems to be optional
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
-    'content-type': 'application/json',
-}
 
-with open("streeteasy/ids.jl") as f:
-    ids = json.loads(f.read())["ids"]
-ids = ids[0]  # take only first one so as not to spam the server
+def introspection_query():
+    # introspection seems not allowed
+    with open(Path(__file__).parent / "introspection.graphql") as f:
+        query = f.read()
+    return query
 
-# introspection seems not allowed
-# with open(Path(__file__).parent / "introspection.graphql") as f:
-#     query = f.read()
 
-with open(Path(__file__).parent / "rental_query.graphql") as f:
-    query = f.read()
+def rentals_query():
+    with open("streeteasy/ids.jl") as f:
+        ids = json.loads(f.read())["ids"]
+    ids = ids[0]  # take only first one so as not to spam the server
+
+    with open(Path(__file__).parent / "rental.graphql") as f:
+        query = f.read()
     
-data_dict = {
-    # "operationName":"rentals",  # seems to be optional
-    "variables": {"ids": ids},
-    "query": query,
-}
+    return ids, query
 
-data_raw = json.dumps(data_dict)
 
-response = httpx.post(
-    'https://api-internal.streeteasy.com/graphql', 
-    headers=headers,
-    content=data_raw,
-    timeout=10,  # longer timeout needed if sending lots of IDs
-)
+def buildings_query():
+    ids = ["422096"]  # example building
 
-data = json.loads(response.text)
+    with open(Path(__file__).parent / "building.graphql") as f:
+        query = f.read()
+    
+    return ids, query
 
-print(json.dumps(data, indent=2))
+    
+def main():
+    headers = {
+        'Origin': 'https://streeteasy.com',  # seems to be optional
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+        'content-type': 'application/json',
+    }
+
+    # ids, query = buildings_query()
+    ids, query = rentals_query()
+
+    data_dict = {
+        # "operationName":"rentals",  # seems to be optional
+        "variables": {"ids": ids},
+        "query": query,
+    }
+
+    data_raw = json.dumps(data_dict)
+
+    response = httpx.post(
+        'https://api-internal.streeteasy.com/graphql', 
+        headers=headers,
+        content=data_raw,
+        timeout=10,  # longer timeout needed if sending lots of IDs
+    )
+
+    data = json.loads(response.text)
+
+    return data
+
+
+if __name__ == "__main__":
+    data = main()
+    print(json.dumps(data, indent=2))
 
