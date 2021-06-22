8b7e1b12a856b63abde57e003fed8ec6ff838845
Robert Engler <rengler33@gmail.com>
2021-06-21 20:51:39 -0500

    Create ListingsItem

--- streeteasy/streeteasy/items.py
+++ streeteasy/streeteasy/items.py
@@ -1,12 +1,30 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy import Field
 
 
-class StreeteasyItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ListingsItem(scrapy.Item):
+    rental_id = Field()
+    anyrooms = Field()
+    anyrooms_description = Field()
+    bedrooms = Field()
+    bedrooms_description = Field()
+    baths_short_description = Field()
+    baths_description = Field()
+    listed_price = Field()
+    title_with_unit = Field()
+    large_image_uri = Field()
+    unit_type = Field()
+    url = Field()
+    building_id = Field()
+    building_url = Field()
+    building_type = Field()
+    building_residential_unit_count = Field()
+    building_year_built = Field()
+    pretty_address = Field()
+    unit = Field()
+    area_id = Field()
+    area_name = Field()
+    contact_id = Field()
+    contact_name = Field()
+    contact_phone = Field()
+    business_name = Field()
